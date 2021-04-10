05349f3f54954d87c9226aef550278d2c923afdf
Egor <egor.haritonov5618@gmail.com>
2021-04-10 23:24:37 +0400

    Настроил Docker-Compose. Приложение работаетю

--- config/config.py
+++ config/config.py
@@ -1,3 +1,4 @@
+import json
 import yaml
 from pymongo import MongoClient
 
--- test_main.py
+++ test_main.py
@@ -2,28 +2,9 @@ import pytest
 import socket as s
 import requests
 from fastapi.testclient import TestClient
-
 from main import app
 
 client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello"}
-
-
-def test_read_item():
-    response = client.get("/employees/")
-    assert response.status_code == 200
-    assert response.json() != ''
-
-
-def test_read_inexistent_item():
-    response = client.get("/employee/")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Not Found"}
 
 
 @pytest.yield_fixture
@@ -34,5 +15,27 @@ def socket():
 
 
 def test_server_connect(socket):
-    socket.connect(('localhost', 8000))
+    socket.connect(('0.0.0.0', 8000))
     assert socket
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello"}
+
+
+def test_read_item():
+    response = client.get("/employees/")
+    employees = []
+    with open("employees.json", 'r') as f:
+        for line in f:
+            employees.append(line)
+    assert response.status_code == 200
+    assert response.json() == {'list_employees': employees}
+
+
+def test_read_inexistent_item():
+    response = client.get("/employee/")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
